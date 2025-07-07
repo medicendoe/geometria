bfa199ac307a143c861b32cb9b88152e46ef7a5d
Gabriel Gallardo <ggallardo@osacontrol.com>
2025-07-07 19:30:25 -0400

    FEAT(Poligon): Adding Poligon implementation
    On the present commit there where made some changes on the headers and
    the implementations was provided.

---
+++ src/Poligon/Poligon.cpp
@@ -0,0 +1,81 @@
+#include "Poligon.h"
+#include "Vector/Vector.h"
+#include <algorithm>
+#include <cmath>
+
+template <typename T>
+Poligon<T>::Poligon(const std::vector<Point<T>>& vertexes) : vertexes(vertexes) {}
+
+template <typename T>
+size_t Poligon<T>::numVertexes() const {
+    return static_cast<size_t>(vertexes.size());
+}
+
+template <typename T>
+T Poligon<T>::shoelace() const {
+    if (vertexes.size() < 3) return false;
+    
+    T sum = T(0);
+    for (size_t i = 0; i < vertexes.size(); ++i) {
+        size_t next = (i + 1) % vertexes.size();
+        
+        // Create vectors from current vertex to previous and next
+        Vector<T> v1(vertexes[i]); 
+        Vector<T> v2(vertexes[next]);
+        
+        // Use cross product from Vector class and extract z coordinate
+        Vector<T> crossResult = v1.cross(v2);
+        sum += crossResult.getZ();
+    }
+
+    return sum;
+
+}
+
+template <typename T>
+bool Poligon<T>::isCCW() const {
+    return shoelace() > T(0);
+}
+
+template <typename T>
+T Poligon<T>::area() const {
+    return std::abs(shoelace()) / T(2);
+}
+
+template <typename T>
+void Poligon<T>::fromCWToCCW() {
+    if (!isCCW()) {
+        std::reverse(vertexes.begin(), vertexes.end());
+    }
+}
+
+template <typename T>
+const Point<T>& Poligon<T>::operator[](size_t index) const {
+    return vertexes[index];
+}
+
+template <typename U>
+std::ostream& operator<<(std::ostream& os, const Poligon<U>& p) {
+    os << "[";
+
+    for (size_t i = 0; i < p.vertexes.size(); ++i) {
+        
+        os << p.vertexes[i]; 
+        
+        if (i < p.vertexes.size() - 1) {
+            os << ", ";
+        }
+    }
+
+    os << "]";
+    
+    return os;
+}
+
+// Explicit template instantiations
+template class Poligon<int>;
+template class Poligon<float>;
+template class Poligon<double>;
+template std::ostream& operator<<<int>(std::ostream& os, const Poligon<int>& p);
+template std::ostream& operator<<<float>(std::ostream& os, const Poligon<float>& p);
+template std::ostream& operator<<<double>(std::ostream& os, const Poligon<double>& p);
--- src/Poligon/Poligon.h
+++ src/Poligon/Poligon.h
@@ -3,18 +3,23 @@
 #include <iostream>
 #include <vector>
 #include "Point/Point.h"
+#include "Vector/Vector.h"
 template <typename T>
 class Poligon {
     public:
         Poligon(const std::vector<Point<T>>& vertexes);
 
-        int numVertexes() const;
+        size_t numVertexes() const;
         bool isCCW() const;
         T area() const;
         void fromCWToCCW();
 
         const Point<T>& operator[](size_t index) const;
+        template <typename U>
+        friend std::ostream& operator<<(std::ostream& os, const Poligono<U>& p);
     private:
         std::vector<Point<T>> vertexes;
-}
+        T shoelace() const;
+
+};
 #endif
